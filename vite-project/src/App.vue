bca74a59e1126c70d67585dd64b6fb6a68a59fba
AlmogSkalka <almogsk14@gmail.com>
2023-01-24 20:38:28 +0200

    updated H1 backColor

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,16 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-  <h1>Welcome to Almog's Website!</h1>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 :style="{ backgroundColor: 'red' }">
+  Welcome to Almog's Website!
+  </h1>
 </template>
 
 <style scoped>
